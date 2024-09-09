db4ae6634ec7915ced5c63715b3ccc996ef7e8c6
josueCastillejos <84791173+211234@users.noreply.github.com>
2024-09-09 11:28:02 -0600

    "Archivos"

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
 import express from "express";
-import 
+import pool from "./config/dbConnection";
+import { envConfig } from "./config/env.config";
+
+const app = express();
+const port = envConfig.port;
+
+app.use(express.json());
+//app.use(productRouter);
+
+pool
+    .connect()
+    .then(() => {
+        console.log("Connected to database");
+    })
+    .catch((err) => {
+        console.error("Database connection error", err);
+    });
+
+app.listen(port, () => {
+    console.log("Server is running on port " + port);
+});
